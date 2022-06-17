d87b6ac8c28a1908f6bdf5e5f20eefc9d3227fa6
Kerim <targetbad@hotmail.com>
2022-06-17 15:50:38 +0300

    Initial Commit

---
+++ SwiftUIInstagramTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIInstagramTutorial
+//
+//  Created by Kerim Civelek on 17.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIInstagramTutorial/SwiftUIInstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInstagramTutorialApp.swift
+//  SwiftUIInstagramTutorial
+//
+//  Created by Kerim Civelek on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
